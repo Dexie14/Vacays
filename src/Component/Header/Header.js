9030628df3f51bba34d936a2365d44095917749e
Adeoniye Adelu <adeluadeoniye14@gmail.com>
2023-04-13 14:56:01 +0100

    webp images

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,85 +1,82 @@
-import React from 'react';
+import React from "react";
 import { NavLink } from "react-router-dom";
 import classes from "./Header.module.css";
 import { useState } from "react";
 import Vacays from "../../assets/VACAYs..svg";
 import menu from "../../assets/menu.svg";
 import close from "../../assets/close.svg";
-import { Link } from 'react-scroll'
+import { Link } from "react-scroll";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-
-    const [isOpen, setIsOpen] = useState(false);
-  
-    const showSideBar = () => setIsOpen(!isOpen);
-
+  const showSideBar = () => setIsOpen(!isOpen);
 
   return (
     <div className={classes.header}>
-        <header>
+      <header>
         <div className={classes.logo}>
           <img src={Vacays} alt="Vacays" />
         </div>
 
-        
-
         {isOpen ? (
-            <div className={`${classes.bar} ${classes.hideBar}`} onClick={showSideBar}>
+          <div
+            className={`${classes.bar} ${classes.hideBar}`}
+            onClick={showSideBar}
+          >
             <img src={close} alt="close" />
-            </div>
-            )
-            : (
-            <div className={classes.ham} onClick={showSideBar}>
+          </div>
+        ) : (
+          <div className={classes.ham} onClick={showSideBar}>
             <img src={menu} alt="menu" />
-            </div>)
-          }
+          </div>
+        )}
 
         <menu className={isOpen ? classes.sidemenu : classes.menu}>
-            <nav className={classes.nav}>
-                <NavLink
-                to="/"
-                className={(navData) =>
-                    navData.isActive ? `${classes.active}` : `${classes.navItems}`
-                  }
-                >
-                    Home
-                </NavLink>
-                <NavLink
-                to="/"
-                className={(navData) =>
-                    navData.isActive ? `${classes.active}` : `${classes.navItems}`
-                  }>
-                    
-                    <Link to = "Destinations"> Destinations   </Link>
-                </NavLink>
-                <NavLink
-                to="/"
-                className={(navData) =>
-                  navData.isActive ? `${classes.active}` : `${classes.navItems}`
-                }>
-                    <Link to = "footer"> About   </Link>
-                     
-                </NavLink>
-                <NavLink
-                to="/"
-                className={(navData) =>
-                    navData.isActive ? `${classes.active}` : `${classes.navItems}`
-                  }>
-                    <Link to = "footer"> Contact   </Link>
-                    
-                </NavLink>
-            </nav>
-            <div className={classes.register} >
-                <button>Register</button>
-            </div>
-            <div className={classes.register}>
-                <button>Sign in</button>
-            </div>
+          <nav className={classes.nav}>
+            <NavLink
+              to="/"
+              className={(navData) =>
+                navData.isActive ? `${classes.active}` : `${classes.navItems}`
+              }
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/"
+              className={(navData) =>
+                navData.isActive ? `${classes.active}` : `${classes.navItems}`
+              }
+            >
+              <Link to="Destinations"> Destinations </Link>
+            </NavLink>
+            <NavLink
+              to="/"
+              className={(navData) =>
+                navData.isActive ? `${classes.active}` : `${classes.navItems}`
+              }
+            >
+              <Link to="footer"> About </Link>
+            </NavLink>
+            <NavLink
+              to="/"
+              className={(navData) =>
+                navData.isActive ? `${classes.active}` : `${classes.navItems}`
+              }
+            >
+              <Link to="footer"> Contact </Link>
+            </NavLink>
+          </nav>
+          <div className={classes.register}>
+            <button>Register</button>
+          </div>
+          <div className={classes.register}>
+            <button>Sign in</button>
+          </div>
         </menu>
-        </header>
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
